c03ead9d20a2628301dcb7f8b8b0459b160ab83f
moajo <mimirosiasd@gmail.com>
2017-01-21 23:53:36 +0900

    item manager mv

---
+++ itemManager.js
@@ -0,0 +1,59 @@
+function ItemManager(name) {
+    this.name = name;
+    this.items = [];
+    this.weights = [];
+    this.$$ = gr("#sea");
+    this.waitZ = 100;
+}
+ItemManager.prototype.register = function (item, weight) {
+    this.weights.push({ name: item, w: weight });
+    for (var j = 0; j < 10; j++) {
+        this.addInstance(item);
+    }
+}
+ItemManager.prototype.addInstance = function (name) {
+    const itemContainer = this.$$(".item-container").first();
+    var node = itemContainer.addChildByName(name, {
+        position: [0, 0, this.waitZ]
+    });
+    node.on("reset", () => {
+        node.setAttribute("position", [0, 0, this.waitZ]);
+    });
+    this.items.push(node);
+    return node;
+}
+ItemManager.prototype.randomPut = function () {
+    var total = 0;
+    this.weights.forEach(function (obj) {
+        total += obj.w;
+    });
+    var k = Math.random() * total;
+    var targetName;
+    for (var i = 0; i < this.weights.length; i++) {
+        if (this.weights[i].w > k) {
+            targetName = this.weights[i].name;
+            break;
+        }
+        k -= this.weights[i].w;
+    }
+    this.set(targetName, Math.random() * 40 - 20);
+}
+
+ItemManager.prototype.set = function (itemName, x) {
+    const camera = this.$$("#main-camera").first();
+    const pos = camera.getAttribute("position");
+    const far = camera.getAttribute("far");
+    var inst;
+    for (var i = 0; i < this.items.length; i++) {
+        var target = this.items[i];
+        var targetPos = target.getAttribute("position");
+
+        if (targetPos.Z > this.waitZ / 2 && target.name.name === itemName) {
+            inst = target;
+            break;
+        } else if (i === this.items.length - 1) {
+            inst = this.addInstance(itemName);
+        }
+    }
+    inst.setAttribute("position", [x ? x : 0, 3, pos.Z - far - 10]);
+}
