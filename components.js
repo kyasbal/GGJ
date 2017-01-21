991b2b4b41f35cf64e904106ae5cbe7dd1a70a32
moajo <mimirosiasd@gmail.com>
2017-01-21 18:58:46 +0900

    fix

--- components.js
+++ components.js
@@ -1,139 +1,149 @@
 const C = {
-    eyeMax: 25,
-    eyeMin: 0,
-    ampl: 3
+  eyeMax: 25,
+  eyeMin: 0,
+  ampl: 3
 };
 let isDobonPlaying = false;
 const Audios = {
-    dobon: new Howl({
-        src: ['./audio/dobon.mp3'],
-        volume: 0.5,
-        onend: function() {
-            isDobonPlaying = false;
-        }
-    })
+  dobon: new Howl({
+    src: ['./audio/dobon.mp3'],
+    volume: 0.5,
+    onend: function () {
+      isDobonPlaying = false;
+    }
+  })
 };
 
 function waveMain(o) {
-    const t = Date.now() / 1000;
-    return Math.sin(o / 50 * Math.PI + t) * C.ampl;
+  // var a = Math.sin(t / 3 * Math.PI * 2)
+  // return a * a * a * a * C.ampl
+  var bigWaveParam = o / 1000 * Math.PI * 2;
+  var bigWave = Math.sin(bigWaveParam);
+  bigWave = bigWave * bigWave;
+  bigWave = bigWave * bigWave;
+  bigWave = bigWave * bigWave;
+  return bigWave * bigWave * bigWave * bigWave * 3 * C.ampl;
+  return bigWave;
+
+  var w1 = Math.sin(o / 57 * Math.PI);
+  var w2 = Math.sin(o / 31 * Math.PI);
+  var w3 = Math.sin(o / 17 * Math.PI);
+  return (w1 + 0.6 * w2 + 0.8 * w3) * C.ampl;
 }
 
 gr.registerComponent("Wave", {
-    attributes: {
-        offset: {
-            converter: "Number",
-            default: 0
-        },
-        yOffset: {
-            converter: "Number",
-            default: 0
-        }
-    },
-    $mount: function() {
-        this.transform = this.node.getComponent("Transform");
-        this.initialY = this.transform.getAttribute("position").Y;
-        this.getAttributeRaw("yOffset").boundTo("yOffset");
-    },
-    $update: function() {
-        const p = this.transform.getAttribute("position");
-        p.Y = waveMain(this.getAttribute("offset")) + this.yOffset;
-        this.transform.setAttribute("position", [p.X, p.Y, p.Z]);
+  attributes: {
+    yOffset: {
+      converter: "Number",
+      default: 0
     }
+  },
+  $mount: function () {
+    this.transform = this.node.getComponent("Transform");
+    this.initialY = this.transform.getAttribute("position").Y;
+    this.getAttributeRaw("yOffset").boundTo("yOffset");
+  },
+  $update: function () {
+    const p = this.transform.getAttribute("position");
+    p.Y = waveMain(p.Z) + this.yOffset;
+    this.transform.setAttribute("position", [p.X, p.Y, p.Z]);
+  },
+  $resetPosition: function () {
+    var count = WAVES.length;
+    var d = 1;
+    var p = this.node.getAttribute("position");
+    this.node.setAttribute("position", [p.X, p.Y, p.Z - count * d]);
+  }
 });
 
 gr.registerComponent("CameraControl", {
-    attributes: {
-        sensibility: {
-            converter: "Number",
-            default: 1.0
-        }
-    },
-    $mount: function() {
-        this.__bindAttributes();
-        this._transform = this.node.getComponent("Transform");
-        document.body.addEventListener("wheel", (e) => {
-            const p = this._transform.getAttribute("position");
-            const y = Math.max(C.eyeMin, Math.min(C.eyeMax, p.Y - e.deltaY * this.sensibility / 100.0));
-            this._transform.setAttribute("position", [p.X, y, p.Z]);
-        });
-    },
-    $update:function(){
-      const p = this._transform.getAttribute("position");
-      this._transform.setAttribute("rotation", `x(-${Math.atan(p.Y/100)}rad)`);
+  attributes: {
+    sensibility: {
+      converter: "Number",
+      default: 1.0
     }
+  },
+  $mount: function () {
+    this.__bindAttributes();
+    this._transform = this.node.getComponent("Transform");
+    document.body.addEventListener("wheel", (e) => {
+      const p = this._transform.getAttribute("position");
+      const y = Math.max(C.eyeMin, Math.min(C.eyeMax, p.Y - e.deltaY * this.sensibility / 100.0));
+      this._transform.setAttribute("position", [p.X, y, p.Z]);
+    });
+  },
+  $update: function () {
+    const p = this._transform.getAttribute("position");
+    this._transform.setAttribute("rotation", `x(-${Math.atan(p.Y/100)}rad)`);
+  }
 });
 
 gr.registerComponent("MoveCameraForward", {
-    attributes: {
-        speed: {
-            converter: "Number",
-            default: 1.0
-        },
-        penalty: {
-            converter: "Number",
-            default: 800
-        }
+  attributes: {
+    speed: {
+      converter: "Number",
+      default: 1.0
     },
-    $mount: function() {
-        this.getAttributeRaw("speed").boundTo("speed");
-        this.getAttributeRaw("penalty").boundTo("penalty");
-        this.lastTime = Date.now();
-        this._transform = this.node.getComponent("Transform");
-        this.li = 0;
-        this.hold = false;
-        this.duration = 0;
-        this.backSpeed = 0;
-    },
-    $update: function() {
-        const t = Date.now();
-        const delta = t - this.lastTime;
-        const p = this._transform.getAttribute("position");
-        const cz = p.Z - delta / 1000. * this.speed;
-        const backIndex = Math.floor((-cz) % 100);
-        if (backIndex !== this.li) {
-            WAVES[this.li].setAttribute("position", [0, 0, Math.floor(cz) - 100]);
-            WAVES[this.li].setAttribute("offset", -Math.floor(cz));
-            this.offset = this.li;
-        }
-
-        if (this.hold) {
-          const y = p.Y + this.backSpeed;
-          this._transform.setAttribute("position",[p.X,y,cz]);
-          this.duration --;
-          if(this.duration<= Date.now()){
-            this.hold = false;
-          }
-        } else {
-            this._transform.setAttribute("position", [p.X, p.Y, cz]);
-            this.lastTime = t;
-            this.li = backIndex;
-            this.order = this.li;
-            if (waveMain(-cz) > p.Y - 2.0) {
-                if (!isDobonPlaying) {
-                    isDobonPlaying = true;
-                    Audios.dobon.play();
-                    $("html,body").animate({
-                        scrollTop: $('body').offset().top
-                    }, this.penalty);
-                    this.hold = true;
-                    this.backSpeed = (C.eyeMax - p.Y) / this.penalty;
-                    this.duration = Date.now() + this.penalty;
-                }
-            }
-        }
+    penalty: {
+      converter: "Number",
+      default: 800
     }
+  },
+  $mount: function () {
+    this.getAttributeRaw("speed").boundTo("speed");
+    this.getAttributeRaw("penalty").boundTo("penalty");
+    this.lastTime = Date.now();
+    this._transform = this.node.getComponent("Transform");
+    this.hold = false;
+    this.duration = 0;
+    this.backSpeed = 0;
+  },
+  $update: function () {
+    const t = Date.now();
+    const delta = t - this.lastTime;
+    const p = this._transform.getAttribute("position");
+    const cz = p.Z - delta / 1000. * this.speed;
+    WAVES.forEach(function (w) {
+      if (w.getAttribute("position").Z > cz) {
+        w.sendMessage("resetPosition");
+      }
+    })
+
+    if (this.hold) {
+      const y = p.Y + this.backSpeed;
+      this._transform.setAttribute("position", [p.X, y, cz]);
+      if (this.duration <= Date.now()) {
+        this.hold = false;
+      }
+    } else {
+      this._transform.setAttribute("position", [p.X, p.Y, cz]);
+      this.lastTime = t;
+      console.log(waveMain(cz));
+      if (waveMain(-cz) > p.Y - 2.0) {
+        if (!isDobonPlaying) {
+          isDobonPlaying = true;
+          Audios.dobon.play();
+          $("html,body").animate({
+            scrollTop: $('body').offset().top
+          }, this.penalty);
+          this.hold = true;
+          this.backSpeed = (C.eyeMax - p.Y) / this.penalty;
+          this.duration = Date.now() + this.penalty;
+        }
+      }
+    }
+  }
 })
 
 gr.registerNode("wave-cube", ["Wave"], {
-    geometry: "wave",
-    scale: "1,1,1",
-    material: "new(lambert)"
+  geometry: "wave",
+  scale: "1,1,1",
+  material: "new(lambert)"
 }, "mesh");
 
 gr.registerNode("scroll-camera", ["CameraControl"], {}, "camera");
 gr.registerNode("apple", ["Wave"], {
-    scale: "0.02",
-    src: "./models/apple.gltf",
-    yOffset: 1
+  scale: "0.02",
+  src: "./models/apple.gltf",
+  yOffset: 1
 }, "model");
--- init.js
+++ init.js
@@ -3,54 +3,59 @@ const sound = new Howl({
   loop: true,
   volume: 0.5
 });
-gr(function() {
-    const $$ = gr("#sea");
-    const waveContainer = $$(".wave-container").get(0);
-    const itemContainer = $$(".item-container").get(0);
-    WAVES = [];
-    for (let i = 0; i < 100; i++) {
-        WAVES.push(waveContainer.addChildByName("wave-cube", {
-            position: `0,0,-${i}`,
-            color: "cyan",
-            offset: i,
-            id: "wave-" + i
-        }));
-    }
-    const apple = new ItemManager('apple');
-    apple.init();
-    setInterval(function() {
-        apple.set(3);
-    }, 1000);
-
+gr(function () {
+  const $$ = gr("#sea");
+  const waveContainer = $$(".wave-container").get(0);
+  const itemContainer = $$(".item-container").get(0);
+  WAVES = [];
+  ITEMS = [];
+  for (let i = 0; i < 100; i++) {
+    WAVES.push(waveContainer.addChildByName("wave-cube", {
+      position: `${Math.random()*3},0,-${i}`,
+      color: "cyan",
+      offset: i,
+      id: "wave-" + i
+    }));
+  }
+  for (let i = 0; i < 8; i++) {
+    ITEMS.push(itemContainer.addChildByName("apple", {
+      position: `0,0,-${i * 100}`
+    }));
+  }
+  const canvas = document.getElementsByTagName("canvas")[0];
+  const box = document.getElementById("gr_container");
+  const pos = (window.innerWidth - canvas.width) / 2;
+  box.style.left = pos + "px";
+  sound.play();
 });
 
 function ItemManager(name) {
-    this.name = name;
-    this.ITEMS = [];
+  this.name = name;
+  this.ITEMS = [];
 }
-ItemManager.prototype.init = function() {
-    const $$ = gr("#sea");
-    const itemContainer = $$(".item-container").first();
-    for (var j = 0; j < 10; j++) {
-        this.ITEMS.push({
-            node: itemContainer.addChildByName(this.name, {
-                position: "0,0,100"
-            }),
-            _flag: false
-        });
-    }
+ItemManager.prototype.init = function () {
+  const $$ = gr("#sea");
+  const itemContainer = $$(".item-container").first();
+  for (var j = 0; j < 10; j++) {
+    this.ITEMS.push({
+      node: itemContainer.addChildByName(this.name, {
+        position: "0,0,100"
+      }),
+      _flag: false
+    });
+  }
 };
-ItemManager.prototype.set = function(x) {
-    const $$ = gr("#sea");
-    const camera = $$("#main-camera").first();
-    const posZ = camera.getAttribute("position").Z;
-    const far = camera.getAttribute("far");
-    for (var i = 0; i < this.ITEMS.length; i++) {
-        if (this.ITEMS[i]._flag === false) {
-            this.ITEMS[i].node.setAttribute("position", x + `,0,${posZ - far}`);
-            this.ITEMS[i]._flag = true;
-            this.ITEMS[i].node.emit("remove", this);
-            break;
-        }
+ItemManager.prototype.set = function (x) {
+  const $$ = gr("#sea");
+  const camera = $$("#main-camera").first();
+  const posZ = camera.getAttribute("position").Z;
+  const far = camera.getAttribute("far");
+  for (var i = 0; i < this.ITEMS.length; i++) {
+    if (this.ITEMS[i]._flag === false) {
+      this.ITEMS[i].node.setAttribute("position", x + `,0,${posZ - far}`);
+      this.ITEMS[i]._flag = true;
+      this.ITEMS[i].node.emit("remove", this);
+      break;
     }
+  }
 }
